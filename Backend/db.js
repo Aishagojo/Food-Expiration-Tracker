96622042ac7edde43952d56b130040deda9d44cc
aishaomaa <124359762+Aishagojo@users.noreply.github.com>
2024-11-04 16:37:57 +0300

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,11 +1,11 @@
-// db.js
+
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',         // Your database host
-    user: 'root',              // Your MySQL username
-    password: '7878', // Your MySQL password
-    database: 'Food_tracker'   // Your database name
+    host: 'localhost',        
+    user: 'root',              
+    password: '--', 
+    database: 'Food_tracker'  
 });
 
 connection.connect((err) => {
